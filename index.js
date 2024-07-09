dfc82fe5308ea3ffd8d9c83701b6eadedddb19d6
Thazar-r <melchior.balthazar@student.moringaschool.com>
2024-07-09 15:13:28 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, configObject)
+      .then(response => response.json())
+      .then(json => {
+        document.body.innerHTML += json.id;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+  
